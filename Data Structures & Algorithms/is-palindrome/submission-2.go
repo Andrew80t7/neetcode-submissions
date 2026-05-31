9751520d92f9aa3cdbc0d83f2f6886a32285913d
Andrew <103926949+Andrew80t7@users.noreply.github.com>
2026-05-31 23:59:31 +0500

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.go
@@ -0,0 +1,26 @@
+func isPalindrome(s string) bool {
+
+	runes := []rune(s)
+	i := 0
+	j := len(runes) - 1
+
+	for i < j {
+
+		if !unicode.IsLetter(runes[i]) && !unicode.IsDigit(runes[i]) {
+			i++
+			continue
+		}
+		if !unicode.IsLetter(runes[j]) && !unicode.IsDigit(runes[j]) {
+			j--
+			continue
+		}
+
+		if unicode.ToLower(runes[i]) != unicode.ToLower(runes[j]) {
+			return false
+		}
+		
+		i++
+		j--
+	}
+	return true
+}
